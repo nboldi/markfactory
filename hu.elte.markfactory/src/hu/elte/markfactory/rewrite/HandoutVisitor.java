baeaa8b19154a349fcf16e25a6c77d3075814e09
nboldi <nboldi@elte.hu>
2015-11-07 21:03:40 +0100

    Generalized and documented handout generation. Solved #6

--- hu.elte.markfactory/src/hu/elte/markfactory/rewrite/HandoutVisitor.java
+++ hu.elte.markfactory/src/hu/elte/markfactory/rewrite/HandoutVisitor.java
@@ -13,7 +13,6 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
@@ -24,12 +23,19 @@ import hu.elte.markfactory.annotations.ExamExercise;
 import hu.elte.markfactory.annotations.ExamTest;
 import hu.elte.markfactory.annotations.TestSolution;
 
+/**
+ * This visitor rewrites the AST to be useful as a handout to students. All the
+ * test-system-specific annotations are removed and all the bodies of the
+ * exercises are commented out.
+ */
 public class HandoutVisitor extends ModificationRecordingVisitor {
 
 	public HandoutVisitor(AST ast) {
 		super(ast);
 	}
 
+	// the sections that need to be commented out (map the beginning to the
+	// end), in reverse order
 	private TreeMap<Integer, Integer> commented = new TreeMap<>(Comparator.reverseOrder());
 	private int removedChars = 0;
 
@@ -52,28 +58,6 @@ public class HandoutVisitor extends ModificationRecordingVisitor {
 		return true;
 	}
 
-	private void registerNodeRemove(ASTNode node) {
-		if (getNextNode(node) != null) {
-			removedChars += getNextNode(node).getStartPosition() - node.getStartPosition();
-		}
-	}
-
-	private static ASTNode getNextNode(ASTNode node) {
-		while (node.getLocationInParent() != null && !node.getLocationInParent().isChildListProperty()) {
-			node = node.getParent();
-		}
-		if (node.getLocationInParent() == null) {
-			return null;
-		}
-		ASTNode parent = node.getParent();
-		List<ASTNode> children = (List<ASTNode>) parent.getStructuralProperty(node.getLocationInParent());
-		int nextIndex = children.indexOf(node) + 1;
-		if (children.size() > nextIndex) {
-			return children.get(nextIndex);
-		}
-		return null;
-	}
-
 	@Override
 	public boolean visit(TypeDeclaration node) {
 		if (removeAnnots(node.modifiers())) {
@@ -90,15 +74,53 @@ public class HandoutVisitor extends ModificationRecordingVisitor {
 		}
 		return true;
 	}
-	
-	private void commentBody(ASTNode node) {
-		int start = node.getStartPosition();
-		commented.put(start - removedChars + 1, start + node.getLength() - removedChars - 1);
+
+	/**
+	 * Registers the removal of the node. This is necessary because otherwise it
+	 * would invalidate the character numbers of the code sections that need to
+	 * be commented out.
+	 */
+	private void registerNodeRemove(ASTNode node) {
+		if (getNextNode(node) != null) {
+			removedChars += getNextNode(node).getStartPosition() - node.getStartPosition();
+		}
 	}
 
+	/**
+	 * Gets the next node in the AST.
+	 */
+	private static ASTNode getNextNode(ASTNode node) {
+		while (node.getLocationInParent() != null && !node.getLocationInParent().isChildListProperty()) {
+			node = node.getParent();
+		}
+		if (node.getLocationInParent() == null) {
+			return null;
+		}
+		ASTNode parent = node.getParent();
+		@SuppressWarnings("unchecked")
+		List<ASTNode> children = (List<ASTNode>) parent.getStructuralProperty(node.getLocationInParent());
+		int nextIndex = children.indexOf(node) + 1;
+		if (children.size() > nextIndex) {
+			return children.get(nextIndex);
+		} else {
+			return getNextNode(parent);
+		}
+	}
+
+	/**
+	 * Comments out a given AST node.
+	 */
 	private void commentNode(ASTNode node) {
 		int start = node.getStartPosition();
 		commented.put(start - removedChars, start + node.getLength() - removedChars);
+	}
+
+	/**
+	 * Comments out the statements in the method body.
+	 */
+	private void commentBody(ASTNode node) {
+		int start = node.getStartPosition();
+		commented.put(start - removedChars + 1, start + node.getLength() - removedChars - 1);
 	}
 
 	private boolean removeAnnots(List<?> modifiers) {
@@ -120,6 +142,9 @@ public class HandoutVisitor extends ModificationRecordingVisitor {
 		String res = super.runOn(root, document);
 		if (res != null) {
 			StringBuilder sb = new StringBuilder(res);
+			// from the end of the file to the beginning
+			// (no need to consider the effect of rewriting on the remaining
+			// positions)
 			for (Entry<Integer, Integer> commentedSection : commented.entrySet()) {
 				sb.insert(commentedSection.getValue(), "*/");
 				sb.insert(commentedSection.getKey(), "/*");
