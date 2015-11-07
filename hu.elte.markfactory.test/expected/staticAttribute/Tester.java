8013e66de14a0e38a2f5a22ade8c5dcca62402d2
nboldi <nboldi@elte.hu>
2015-11-07 21:09:17 +0100

    Updated test for static attributes

--- hu.elte.markfactory.test/expected/staticAttribute/Tester.java
+++ hu.elte.markfactory.test/expected/staticAttribute/Tester.java
@@ -4,11 +4,14 @@ import hu.elte.markfactory.annotations.ExamTest;
 @ExamTest
 public class Tester extends hu.elte.markfactory.testbase.ReflectionTester {
 
+	private static final String DO_NOT_TOUCH = "no-go";
+	
 	@ExamExercise
 	public static void test() {
 		
 		try {
 			String a = ((java.lang.String) staticFieldValue("Tested", "CONST"));
+			String b = Tester.DO_NOT_TOUCH;
 		} catch (hu.elte.markfactory.testbase.MissingProgramElementException e) {
 			output(e);
 			return;
--- hu.elte.markfactory.test/tests/staticAttribute/Tester.java
+++ hu.elte.markfactory.test/tests/staticAttribute/Tester.java
@@ -3,10 +3,13 @@ import hu.elte.markfactory.annotations.ExamTest;
 
 @ExamTest
 public class Tester {
+	
+	private static final String DO_NOT_TOUCH = "no-go";
 
 	@ExamExercise
 	public static void test() {
 		String a = Tested.CONST;
+		String b = Tester.DO_NOT_TOUCH;
 	}
 	
 }
